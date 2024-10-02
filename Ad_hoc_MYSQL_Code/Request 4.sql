ae5439d638f80f59f83d87a7a68cc76b53722759
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:57:58 +0530

    Request 4.sql

--- Ad_hoc_MYSQL_Code/Request 4.sql
+++ Ad_hoc_MYSQL_Code/Request 4.sql
@@ -1,12 +1,19 @@
-with increase_in_product_by_segment as (
-select d.segment, count(distinct case when f.fiscal_year = 2020 then f.product_code end) as unique_product_2020,
-count(distinct case when f.fiscal_year = 2021 then f.product_code end) as unique_product_2021
-from fact_sales_monthly f
-join dim_product  d
-on f.product_code = d.product_code
-group by segment
-
+WITH increase_in_product_by_segment AS (
+    SELECT 
+        d.segment, 
+        COUNT(DISTINCT CASE WHEN f.fiscal_year = 2020 THEN f.product_code END) AS unique_product_2020,
+        COUNT(DISTINCT CASE WHEN f.fiscal_year = 2021 THEN f.product_code END) AS unique_product_2021
+    FROM 
+        fact_sales_monthly f
+    JOIN 
+        dim_product d ON f.product_code = d.product_code
+    GROUP BY 
+        d.segment
 )
-select *, (unique_product_2021 - unique_product_2020) as difference 
-from increase_in_product_by_segment
-order by difference desc;
+SELECT 
+    *, 
+    (unique_product_2021 - unique_product_2020) AS difference 
+FROM 
+    increase_in_product_by_segment
+ORDER BY 
+    difference DESC;
