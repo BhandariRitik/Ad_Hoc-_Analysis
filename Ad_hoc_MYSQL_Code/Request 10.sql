53c871bab0abde09804766af993fed514e66e371
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:56:41 +0530

    Request 10.sql

--- Ad_hoc_MYSQL_Code/Request 10.sql
+++ Ad_hoc_MYSQL_Code/Request 10.sql
@@ -1,16 +1,30 @@
-with cte1 as (
-select division, product_code, CONCAT(product, ' (',variant, ')') as product, sum(sold_quantity) as
-total_sold_quantity
-from gross_sales
-where fiscal_year = 2021
-group by division,product_code,product, variant
+WITH cte1 AS (
+    SELECT 
+        division, 
+        product_code, 
+        CONCAT(product, ' (', variant, ')') AS product, 
+        SUM(sold_quantity) AS total_sold_quantity
+    FROM 
+        gross_sales
+    WHERE 
+        fiscal_year = 2021
+    GROUP BY 
+        division, 
+        product_code, 
+        product, 
+        variant
 ),
-cte2 as (
-select *, dense_rank() over(partition by division order by total_sold_quantity desc) 
-as rank_order
-from cte1
+cte2 AS (
+    SELECT 
+        *, 
+        DENSE_RANK() OVER (PARTITION BY division ORDER BY total_sold_quantity DESC) AS rank_order
+    FROM 
+        cte1
 )
-select * from cte2 
-where rank_order <=3;
-
+SELECT 
+    * 
+FROM 
+    cte2 
+WHERE 
+    rank_order <= 3;
 
