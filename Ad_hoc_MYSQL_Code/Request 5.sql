7d9716385e93e872981198c60208ed42be0aae99
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:58:32 +0530

    Request 5.sql

--- Ad_hoc_MYSQL_Code/Request 5.sql
+++ Ad_hoc_MYSQL_Code/Request 5.sql
@@ -1,13 +1,27 @@
-(select f.product_code, d.product, f.manufacturing_cost
-from fact_manufacturing_cost f
-join dim_product d
-on f.product_code = d.product_code
-order by manufacturing_cost  desc
-limit 1)
-union
-(select f.product_code, d.product, f.manufacturing_cost
-from fact_manufacturing_cost f
-join dim_product d
-on f.product_code = d.product_code
-order by manufacturing_cost  asc
-limit 1);
+(
+    SELECT 
+        f.product_code, 
+        d.product, 
+        f.manufacturing_cost
+    FROM 
+        fact_manufacturing_cost f
+    JOIN 
+        dim_product d ON f.product_code = d.product_code
+    ORDER BY 
+        manufacturing_cost DESC
+    LIMIT 1
+)
+UNION
+(
+    SELECT 
+        f.product_code, 
+        d.product, 
+        f.manufacturing_cost
+    FROM 
+        fact_manufacturing_cost f
+    JOIN 
+        dim_product d ON f.product_code = d.product_code
+    ORDER BY 
+        manufacturing_cost ASC
+    LIMIT 1
+);
