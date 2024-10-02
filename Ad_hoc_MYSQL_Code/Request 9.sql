5e52d909e95454073aff8b1ee261ff45629f67bf
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 19:00:16 +0530

    Request 9.sql

--- Ad_hoc_MYSQL_Code/Request 9.sql
+++ Ad_hoc_MYSQL_Code/Request 9.sql
@@ -1,18 +1,31 @@
-with cte1 as 
-(
-select c.channel, round(sum(f.sold_quantity * fg.gross_price)/1000000,2) as gross_sales_mln from
-fact_sales_monthly f
-join fact_gross_price fg
-on f.product_code = fg.product_code
-join dim_customer c
-on f.customer_code = c.customer_code
-where f.fiscal_year = 2021
-group by channel
+WITH cte1 AS (
+    SELECT 
+        c.channel, 
+        ROUND(SUM(f.sold_quantity * fg.gross_price) / 1000000, 2) AS gross_sales_mln
+    FROM 
+        fact_sales_monthly f
+    JOIN 
+        fact_gross_price fg ON f.product_code = fg.product_code
+    JOIN 
+        dim_customer c ON f.customer_code = c.customer_code
+    WHERE 
+        f.fiscal_year = 2021
+    GROUP BY 
+        c.channel
 ),
-cte2 as (
-select channel, gross_sales_mln, sum(gross_sales_mln) over() as total_sales
-from cte1
+cte2 AS (
+    SELECT 
+        channel, 
+        gross_sales_mln, 
+        SUM(gross_sales_mln) OVER () AS total_sales
+    FROM 
+        cte1
 )
-select channel,gross_sales_mln, round((gross_sales_mln/total_sales)*100,2) as percentage
-from cte2
-order by percentage desc;
+SELECT 
+    channel, 
+    gross_sales_mln, 
+    ROUND((gross_sales_mln / total_sales) * 100, 2) AS percentage
+FROM 
+    cte2
+ORDER BY 
+    percentage DESC;
