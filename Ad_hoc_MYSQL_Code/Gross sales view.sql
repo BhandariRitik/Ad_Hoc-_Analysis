5cb4903e91c304c5048476ffbe08189078b4284a
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:55:40 +0530

    Gross sales view.sql

--- Ad_hoc_MYSQL_Code/Gross sales view.sql
+++ Ad_hoc_MYSQL_Code/Gross sales view.sql
@@ -1,12 +1,28 @@
-
-select fs.date, fs.fiscal_year, fs.customer_code, d.customer,  d.channel, d.market, d.region,
-fs.product_code, dp.product, dp.division, dp.segment, dp.variant, fs.sold_quantity, fg.gross_price, 
-round((fs.sold_quantity * fg.gross_price),2) as gross_sales_amount
-from fact_sales_monthly fs
-join dim_customer d 
-on fs.customer_code = d.customer_code
-join dim_product dp
-on fs.product_code = dp.product_code
-join fact_gross_price fg
-on fs.product_code = fg.product_code and 
-fs.fiscal_year = fg.fiscal_year;
+SELECT 
+    fs.date, 
+    fs.fiscal_year, 
+    fs.customer_code, 
+    d.customer,  
+    d.channel, 
+    d.market, 
+    d.region,
+    fs.product_code, 
+    dp.product, 
+    dp.division, 
+    dp.segment, 
+    dp.variant, 
+    fs.sold_quantity, 
+    fg.gross_price, 
+    ROUND((fs.sold_quantity * fg.gross_price), 2) AS gross_sales_amount
+FROM 
+    fact_sales_monthly fs
+JOIN 
+    dim_customer d 
+    ON fs.customer_code = d.customer_code
+JOIN 
+    dim_product dp
+    ON fs.product_code = dp.product_code
+JOIN 
+    fact_gross_price fg
+    ON fs.product_code = fg.product_code 
+    AND fs.fiscal_year = fg.fiscal_year;
